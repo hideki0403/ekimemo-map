190dece4690eb70bd50be4375c9305eb9d7f5441
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-30 20:46:52 +0900

    enhance: 自動生成するテーマカラーを追加

--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -16,6 +16,11 @@ export function applyTheme() {
         'accent-lighten': tinycolor(theme.accent).lighten(10).toString(),
         'accent-darken': tinycolor(theme.accent).darken(10).toString(),
         'accent-background': tinycolor(theme.accent).setAlpha(0.15).toString(),
+        'panel-background': tinycolor(theme.foreground).setAlpha(0.15).toString(),
+        'warning-background': tinycolor(theme.warning).setAlpha(0.15).toString(),
+        'error-background': tinycolor(theme.error).setAlpha(0.15).toString(),
+        'info-background': tinycolor(theme.info).setAlpha(0.15).toString(),
+        'success-background': tinycolor(theme.success).setAlpha(0.15).toString(),
     }
 
     Object.assign(theme, additionalColors)
