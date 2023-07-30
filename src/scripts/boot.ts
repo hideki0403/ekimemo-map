d09c41b9d40258031086a0287d2b7a7b9157e854
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-30 20:51:10 +0900

    enhance: 1日おきに駅情報の更新がないかを確認するように

--- src/scripts/boot.ts
+++ src/scripts/boot.ts
@@ -1,6 +1,19 @@
 import * as themeManager from './theme'
+import * as updator from './updator'
+import { stateStore } from './store'
+import dayjs from 'dayjs'
 
-export default function() {
+export default async function() {
     themeManager.watchThemeChange()
     themeManager.applyTheme()
+
+    if (dayjs().diff(dayjs(stateStore.get('lastCheck')), 'day') > 1) {
+        const result = await updator.check().catch((e) => console.error(e))
+        if (result instanceof Error || !result) return
+        if (result.updateAvailable) {
+            // TODO: トースト通知を出す
+            console.log(`Station data update available (v${result.version}, ${result.size} bytes)`)
+            const updateResult = await updator.execute()
+        }
+    }
 }
