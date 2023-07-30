a9f6196f5c788a0ce09a2d4f318c631c1047d817
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-30 20:48:33 +0900

    refactor: 冗長だった箇所を改善

--- src/scripts/updator.ts
+++ src/scripts/updator.ts
@@ -2,29 +2,24 @@ import * as database from './database'
 import { stateStore } from './store'
 
 export async function execute() {
-    let lines: any[] = []
-    let stations: any[] = []
+    let data: any
 
     try {
-        const links = {} as Record<string, string>
-        const response = await (await fetch('https://api.github.com/repos/Seo-4d696b75/station_database/contents/out/main')).json()
-        response.forEach((file: { name: string, size: number, download_url: string }) => {
-            if (['station.json', 'line.json'].includes(file.name)) {
-                links[file.name.replace('.json', '')] = file.download_url
-            }
-        })
-
-        lines = await (await fetch(links.line)).json()
-        stations = await (await fetch(links.station)).json()
+        data = await (await fetch('https://raw.githubusercontent.com/Seo-4d696b75/station_database/main/out/main/data.json')).json()
     } catch(e) {
-        console.error(e)
+        console.error('Update failed', e)
         return false
     }
     
     await database.cache.lines.clear()
-    await database.cache.lines.bulkPut(lines)
+    await database.cache.lines.bulkPut(data.lines)
     await database.cache.stations.clear()
-    await database.cache.stations.bulkPut(stations)
+    await database.cache.stations.bulkPut(data.stations)
+    await database.cache.treeSegments.clear()
+    await database.cache.treeSegments.bulkPut(data.tree_segments)
+
+    stateStore.set('currentVersion', data.version)
+    console.log(`Station data updated to v${data.version}`)
 
     return true
 }
@@ -32,16 +27,18 @@ export async function execute() {
 export async function check() {
     let result
     try {
-        result = await (await fetch('https://api.github.com/repos/Seo-4d696b75/station_database/releases/latest')).json()
+        result = await (await fetch('https://raw.githubusercontent.com/Seo-4d696b75/station_database/main/latest_info.json')).json()
     } catch {
         return new Error('Failed to check update')
     }
     
-    const latestVersion = result.tag_name
+    const latestVersion = result.version
 
     stateStore.set('lastCheck', new Date().toISOString())
     return {
         updateAvailable: latestVersion !== stateStore.get('currentVersion'),
-        latestVersion,
+        version: latestVersion,
+        url: result.url,
+        size: result.size,
     }
 }
