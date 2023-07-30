94a58dafe37843277628c467ac2f0184346fb032
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-30 20:51:37 +0900

    feat: メインページを仮実装

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,41 @@
 <template>
+    <MeButton :primary="isGpsActive" @click="toggleGps">
+        <CurrentLocationIcon v-if="!isGpsActive" />
+        <CurrentLocationOffIcon v-else />
+        位置情報{{ isGpsActive ? 'ON' : 'OFF' }}
+    </MeButton>
+
+    <MeButton @click="search">検索</MeButton>
+
+    <div class="_gap">
+        <MeStationSimple v-for="data in stations" :key="data.station.id" :data="data" />
+    </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import { CurrentLocationIcon, CurrentLocationOffIcon } from 'vue-tabler-icons'
+import gps from '@/scripts/gps'
+import Station from '@/scripts/station'
+import MeButton from '@/components/MeButton.vue'
+import MeStationSimple from '@/components/MeStationSimple.vue'
+
+const isGpsActive = gps.isActive
+const stations = Station.getReactive
+
+function toggleGps() {
+    if (isGpsActive.value) {
+        gps.stopWatch()
+    } else {
+        gps.startWatch((x) => {
+            Station.updateLocation(x.coords.latitude, x.coords.longitude)
+        })
+    }
+}
+
+function search() {
+    gps.getCurrentPosition().then(async x => {
+        Station.updateLocation(x.coords.latitude, x.coords.longitude)
+    })
+}
+
 </script>
