3de986e8f7d52ce89f90ff9fc0bc4f0b7de31b19
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-07-30 23:28:50 +0900

    feat: PWAに対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,19 +10,35 @@ export default defineNuxtConfig({
   ],
   modules: [
     '@nuxtjs/tailwindcss',
-    [
-      '@nuxtjs/google-fonts',
-      {
-        families: {
-          'Noto+Sans+JP': true,
-        },
-        display: 'swap',
-        inject: true,
-      }
-    ],
+    '@nuxtjs/google-fonts',
     '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt'
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vite-pwa/nuxt',
   ],
+  pwa: {
+    registerType: 'autoUpdate',
+    registerWebManifestInRouteRules: true,
+    manifest: {
+      name: '駅サーチ',
+      short_name: '駅サーチ',
+      description: '近くにある駅一覧を確認できるWebアプリ',
+    },
+    workbox: {
+      navigateFallback: '/',
+      importScripts: ['static/sw.js']
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    }
+  },
+  googleFonts: {
+    families: {
+      'Noto+Sans+JP': true,
+    },
+    display: 'swap',
+    inject: true,
+  },
   devServer: {
     port: 3001
   }
