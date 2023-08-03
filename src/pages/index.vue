9f24fa75d2d2673fe25a619e5b22f4c70933aa4f
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-08-03 19:48:30 +0900

    feat: 更新時間や座標を表示するように

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,13 +7,20 @@
 
     <MeButton @click="search">検索</MeButton>
 
+    <div>
+        <div>{{ currentCoords.latitude }}, {{ currentCoords.longitude }} (精度: {{ beautifyScale(currentCoords.accuracy) }})</div>
+        <div>最終更新: {{ currentCoords.updatedAt }}</div>
+    </div>
+
     <div class="_gap">
         <MeStationSimple v-for="data in stations" :key="data.station.id" :data="data" />
     </div>
 </template>
 
 <script lang="ts" setup>
+import dayjs from 'dayjs'
 import { CurrentLocationIcon, CurrentLocationOffIcon } from 'vue-tabler-icons'
+import { beautifyScale } from '@/scripts/utils'
 import gps from '@/scripts/gps'
 import Station from '@/scripts/station'
 import MeButton from '@/components/MeButton.vue'
@@ -22,11 +29,25 @@ import MeStationSimple from '@/components/MeStationSimple.vue'
 const isGpsActive = gps.isActive
 const stations = Station.getReactive
 
+const currentCoords = ref({
+    latitude: 0,
+    longitude: 0,
+    accuracy: 0,
+    updatedAt: '',
+})
+
 function toggleGps() {
     if (isGpsActive.value) {
         gps.stopWatch()
     } else {
         gps.startWatch((x) => {
+            currentCoords.value = {
+                latitude: x.coords.latitude,
+                longitude: x.coords.longitude,
+                accuracy: x.coords.accuracy,
+                updatedAt: dayjs().format('HH:mm:ss'),
+            }
+
             Station.updateLocation(x.coords.latitude, x.coords.longitude)
         })
     }
